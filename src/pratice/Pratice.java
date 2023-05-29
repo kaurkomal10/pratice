82df7c86ce0e0d675d6f645288f81d64f4f482d3
kaurkomal10 <133712444+kaurkomal10@users.noreply.github.com>
2023-05-29 14:54:18 -0400

    Update Pratice.java

--- src/pratice/Pratice.java
+++ src/pratice/Pratice.java
@@ -16,6 +16,7 @@ public class Pratice {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("komalllllllllssssssssss");
+        // komal
     }
     
 }
